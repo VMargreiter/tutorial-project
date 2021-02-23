f175332709c7996fa5a459bcd3826e2643cf3495
Valentin Margreiter <32058983+VMargreiter@users.noreply.github.com>
2021-02-23 17:13:45 +0100

    style adaptions

---
+++ src/templates/homepage.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Layout from "../components/layout"
+import './src/styles.scss'
+
+const IndexPage = () => (
+  <Layout>
+    <h1 class="hero-text">Hey gang</h1>
+    <p>Welcome to your new Gatsby site.</p>
+  </Layout>
+)
+
+export default IndexPage
