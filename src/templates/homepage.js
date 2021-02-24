05c8d520b8d10e74d27686bcd2c73374e4668e36
Valentin Margreiter <32058983+VMargreiter@users.noreply.github.com>
2021-02-24 11:12:14 +0100

    Move scss to templates folder
    Gatsby fails to correctly detect it otherwise

--- src/templates/homepage.js
+++ src/templates/homepage.js
@@ -1,12 +1,26 @@
 import React from "react"
 import Layout from "../components/layout"
-import './src/styles.scss'
+import { graphql } from "gatsby"
+import './styles.scss'
 
-const IndexPage = () => (
-  <Layout>
-    <h1 class="hero-text">Hey gang</h1>
-    <p>Welcome to your new Gatsby site.</p>
-  </Layout>
-)
 
-export default IndexPage
+const homepage = () => {
+  return(
+    <Layout>
+      <h1 class="hero-text">Hey gang</h1>
+      <p>Welcome to your new Gatsby site.</p>
+    </Layout>
+  )
+}
+
+export const allPages = graphql`
+  query allPages {
+    allWpPage {
+      nodes {
+        id
+      }
+    }
+  }
+`
+
+export default homepage
